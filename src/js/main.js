90ef524f96cbfe32ac85b2c75f6ff306b0d8f234
dolonche <ivan.pitenin@gmail.com>
2018-03-14 14:06:39 +0300

    continue work

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,35 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
-        }
+$('.owl-carousel.owl-carousel--galery').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 3
     }
+  }
+})
+$('.owl-carousel.owl-carousel--main').owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
 })
